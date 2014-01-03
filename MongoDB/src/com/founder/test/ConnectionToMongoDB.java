6562790182a78c0433448856dc3d4fd767e62650
wuyongwen <wuyongwen@gmail.com>
2014-01-03 16:19:04 +0800

    Signed-off-by: wuyongwen <wuyongwen@gmail.com>

--- MongoDB/src/com/founder/test/ConnectionToMongoDB.java
+++ MongoDB/src/com/founder/test/ConnectionToMongoDB.java
@@ -1,7 +1,18 @@
 package com.founder.test;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+
 public class ConnectionToMongoDB {
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws Exception {
+		Mongo mongo = MongoDBUtil.connect("localhost", 27017);
+		DB db = mongo.getDB("prefs");
+		DBCollection collection = db.getCollection("location");
+		DBCursor cur = collection.find();
+		while(cur.hasNext()){
+			System.out.println(cur.next());
+		}
 	}
 }
