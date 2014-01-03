a12395aae19a22d2a0245bf85f77bbeeea9c3b05
wuyongwen <wuyongwen@gmail.com>
2014-01-03 18:23:07 +0800

    Signed-off-by: wuyongwen <wuyongwen@gmail.com>

--- MongoDB/src/com/founder/test/ConnectionToMongoDB.java
+++ MongoDB/src/com/founder/test/ConnectionToMongoDB.java
@@ -1,5 +1,9 @@
 package com.founder.test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -8,11 +12,11 @@ import com.mongodb.Mongo;
 public class ConnectionToMongoDB {
 	public static void main(String[] args) throws Exception {
 		Mongo mongo = MongoDBUtil.connect("localhost", 27017);
-		DB db = mongo.getDB("prefs");
-		DBCollection collection = db.getCollection("location");
-		DBCursor cur = collection.find();
-		while(cur.hasNext()){
-			System.out.println(cur.next());
+		List<String>  list= mongo.getDatabaseNames();
+		for(String s: list){
+			DB db = mongo.getDB(s);
+			Set<String> collections = db.getCollectionNames();
+			System.out.println(Arrays.toString(collections.toArray()));
 		}
 	}
 }
